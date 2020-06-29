3637714ad8cc70b81fa13d94d83cbf901cf47770
Hanake0 <bghaesee@outlook.com>
2020-06-29 00:56:00 -0300

    say

---
+++ Comandos/utilidades/diga.js
@@ -0,0 +1,25 @@
+const { Command } = require('discord.js-commando');
+
+module.exports = class SayCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'diga',
+			aliases: ['say', 'echo'],
+			group: 'utilidades',
+			memberName: 'diga',
+			description: 'Responde com a mensagem designada e apaga a mensagem original.',
+			args: [
+				{
+					key: 'mensagem',
+					prompt: 'O que você quer que o bot diga?',
+					type: 'string',
+				},
+			],
+		});
+	}
+
+	async run(message, { mensagem }) {
+		await msg.delete(5)
+		msg.say (mensagem);
+	}
+};
