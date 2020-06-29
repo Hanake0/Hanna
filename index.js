eb62fca584fc3d4322410cde0166204e5e79d4d2
bghaesebgh <bghaese.bgh@gmail.com>
2020-06-29 01:47:29 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const { CommandoClient } = require('discord.js-commando');
+const path = require('path');
+
+const client = new CommandoClient({
+	commandPrefix: 'h',
+	owner: '380512056413257729',
+	unknownCommandResponse: false,
+});
+
+client.registry
+	.registerDefaultTypes()
+	.registerGroups([
+	  ['basicos', 'Comandos padrões do Commando'],
+		['utilidades', 'Ainda em testes'],
+	])
+	.registerDefaultGroups({
+	  util: false,
+	})
+	.registerDefaultCommands()
+	.registerCommandsIn(path.join(__dirname, 'Comandos'));
+
+client.once('ready', () => {
+	console.log(`Logged in as ${client.user.tag}! (${client.user.id})`);
+	client.user.setActivity('with Commando');
+});
+
+client.on('error', console.error);
+
+client.login('process.env.AUTH_TOKEN');
