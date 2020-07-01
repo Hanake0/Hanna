be69b5d3a9e7f8a20ed1bf4b46752e455dd09150
Hanake0 <bghaesee@outlook.com>
2020-07-01 12:28:41 -0300

    anunciar e diga

---
+++ Comandos/utilidades/anunciar.js
@@ -0,0 +1,37 @@
+const { Command } = require('../../commando discord.js-v12/src/index.js');
+
+module.exports = class AnunciarCommand extends Command {
+	constructor(client) {
+		super(client, {
+			name: 'anunciar',
+			aliases: ['digacanal', 'repita'],
+			group: 'utilidades',
+			memberName: 'anunciar',
+			clientPermissions: ['MANAGE_MESSAGES'],
+			description: 'Responde com a mensagem designada no canal designado e apaga a mensagem original.',
+			args: [
+			  {
+			    key: 'canal',
+			    prompt: 'em que canal você quer que o bot fale?',
+			    type: 'channel',
+			    default: '',
+			  },
+				{
+					key: 'mensagem',
+					prompt: 'O que você quer que o bot diga?',
+					type: 'string',
+				},
+			],
+			argsPromptLimit: 2,
+		});
+	}
+
+	async run(message, { canal, mensagem }) {
+	  if (clientPermissions.has("SEND_MESSAGES")) {
+	    await message.delete(5);
+	    canal.send(mensagem);
+	  } else {
+	    message.say('Desculpe, mas eu não tenho permissão para enviar mensagens nesse canal')
+	  }
+	}
+};
--- Comandos/utilidades/diga.js
+++ Comandos/utilidades/diga.js
@@ -1,40 +1,27 @@
 const { Command } = require('../../commando discord.js-v12/src/index.js');
 
-module.exports = class SayCommand extends Command {
-	constructor(client) {
-		super(client, {
-			name: 'diga',
-			aliases: ['say', 'echo'],
-			group: 'utilidades',
-			memberName: 'diga',
-			clientPermissions: ['MANAGE_MESSAGES'],
-			description: 'Responde com a mensagem designada e apaga a mensagem original.',
-			args: [
-			  {
-			    key: 'canal',
-			    prompt: 'em que canal você quer que o bot fale?',
-			    type: 'channel',
-			    default: '',
-			  },
-				{
-					key: 'mensagem',
-					prompt: 'O que você quer que o bot diga?',
-					type: 'string',
+module.exports = class DigaCommand extends Command {
+  constructor(client) {
+    super(client, {
+      name: 'diga',
+      aliases: ['echo', 'anonimo'],
+      group: 'utilidades',
+      memberName: 'diga',
+      clientPermissions: ['MANAGE_MESSAGES'],
+      description: 'Responde com a mensagem designada e apaga a mensagem original.',
+      args: [
+        {
+          key: 'mensagem',
+          prompt: 'O que você quer que o bot diga?',
+          type: 'string',
 				},
 			],
-			argsPromptLimit: 0,
-			pattern: [`canal mensagem`, `mensagem`],
-		});
-	}
+      argsPromptLimit: 0,
+    });
+  }
 
-	async run(message, { canal, mensagem }) {
-	  if (canal === '') {
-  	  await message.delete(5);
-	  	message.say(mensagem);
-	  }
-	  else {
-	    await message.delete(5);
-	    canal.send(mensagem);
-	  }
-	}
+  async run(message, { mensagem }) {
+    await message.delete(5);
+    message.say(mensagem);
+  }
 };
