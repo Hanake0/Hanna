47dd77489533d31ff9a3199da8c970b36811a992
Hanake0 <bghaesee@outlook.com>
2020-07-03 20:54:12 -0300

    permissions fix

--- Comandos/adm/addperm.js
+++ Comandos/adm/addperm.js
@@ -1,5 +1,5 @@
 const { Command } = require('../../commando discord.js-v12/src/index.js');
-const Discord = require('discord.js');
+const { Permissions } = require('discord.js');
 
 module.exports = class AddPermCommand extends Command {
   constructor(client) {
