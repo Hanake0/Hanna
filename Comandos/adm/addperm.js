eda55c344e6f3f3ea07500967680680ff50d653f
Hanake0 <bghaesee@outlook.com>
2020-07-03 20:28:33 -0300

    tester

--- Comandos/adm/addperm.js
+++ Comandos/adm/addperm.js
@@ -58,7 +58,7 @@ module.exports = class AddPermCommand extends Command {
         configurable: true
       });
     };
-    message.say(`${permsF}`);
+    console.log(`${permsF}`);
     if (addRem === 'add') {
       message.channel.overwritePermissions(usuário.id, permsT);
       message.say(`Permissões adicionadas com sucesso para o usuário ${usuário.username}`);
