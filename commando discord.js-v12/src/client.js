9951854c5845804c49a94f07145517e90970b83d
Breno G H <bghaesee@outlook.com>
2020-07-24 08:10:29 -0300

    fix

--- commando discord.js-v12/src/client.js
+++ commando discord.js-v12/src/client.js
@@ -65,8 +65,7 @@ class CommandoClient extends discord.Client {
 
 		// Set up command handling
 		const msgErr = err => { this.emit('error', err); };
-		this.on('message', message => {
-			this.dispatcher.handleMessage(message).catch(msgErr); });
+		this.on('message', message => { this.dispatcher.handleMessage(message).catch(msgErr); });
 		this.on('messageUpdate', (oldMessage, newMessage) => {
 			this.dispatcher.handleMessage(newMessage, oldMessage).catch(msgErr);
 		});
