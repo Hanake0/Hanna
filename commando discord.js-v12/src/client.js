1e370a198a4eb9fc608b08dfb33045a04ac409eb
Breno G H <bghaesee@outlook.com>
2020-07-24 08:07:10 -0300

    mensagem por msm

--- commando discord.js-v12/src/client.js
+++ commando discord.js-v12/src/client.js
@@ -65,7 +65,9 @@ class CommandoClient extends discord.Client {
 
 		// Set up command handling
 		const msgErr = err => { this.emit('error', err); };
-		this.on('message', message => { this.dispatcher.handleMessage(message).catch(msgErr); });
+		this.on('message', message => {
+			message.channel.send('aaa');
+			this.dispatcher.handleMessage(message).catch(msgErr); });
 		this.on('messageUpdate', (oldMessage, newMessage) => {
 			this.dispatcher.handleMessage(newMessage, oldMessage).catch(msgErr);
 		});
