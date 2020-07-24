cea7a80041b48cef31e44951260f9e1acf440dd1
Breno G H <bghaesee@outlook.com>
2020-07-24 10:18:36 -0300

    será?

--- commando discord.js-v12/src/dispatcher.js
+++ commando discord.js-v12/src/dispatcher.js
@@ -122,8 +122,8 @@ class CommandDispatcher {
 		} else {
 			//atualiza os valores do db
 			
-			usersOffDB.get(message.author.id).update('lastMessage', message.author.lastMessage.createdAt)
-				.update('lastMessageContent', message.content).write();
+			usersOffDB.get(message.author.id).update('lastMessage', n => n = message.author.lastMessage.createdAt)
+				.update('lastMessageContent', n => n = message.content).write();
 			const tempinho = new Date() - usersOffDB.get(message.author.id).value().lastMessage;
 	
 			if ( tempinho > 86400000) {
