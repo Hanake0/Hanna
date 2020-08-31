944282de1612bceb38612ceda11f20d938729668
Breno G H <bghaesee@outlook.com>
2020-08-31 17:52:09 -0300

    ADDED: guildMemberRemove.js

---
+++ Eventos/guildMemberRemove.js
@@ -0,0 +1,21 @@
+module.exports = async (client, membro) => {
+  const id = membro.guild.id;
+  
+  const WC = '698560208309452810';
+  const Wclub = client.guilds.cache.get(WC);
+
+  const WS = '749990911802474667';
+  const Wstore = client.guilds.cache.get(WS);
+  
+  const roleID = '750073380711170142';
+  const role = Wstore.roles.cache.get(roleID);
+
+  // verifica de que servidor o membro saiu e remove o cargo
+  switch (id) {
+    case WC:
+      if (Wstore.members.cache.has(membro.id)) {
+        const membro2 = Wstore.members.cache.get(membro.id);
+        membro2.roles.remove(role);
+      }
+  }
+}
