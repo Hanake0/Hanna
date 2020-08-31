30a9e2548f19a5ddf2abcf43bfd185bafd934654
Breno G H <bghaesee@outlook.com>
2020-08-31 17:34:21 -0300

    fix typo in guildMemberAdd.js²

--- Eventos/guildMemberAdd.js
+++ Eventos/guildMemberAdd.js
@@ -1,19 +1,22 @@
 module.exports = async (client, membro) => {
   const id = membro.guild.id;
+  
   const WC = '698560208309452810';
   const Wclub = client.guilds.cache.get(WC);
+
   const WS = '749990911802474667';
   const Wstore = client.guilds.cache.get(WS);
+  
   const roleID = '750073380711170142';
   const role = client.guilds.get(Wstore).roles.cache.get(roleID);
 
   // verifica em que servidor o membro entrou e adiciona o cargo se ele estiver no outro
   switch (id) {
-    case Wstore:
+    case WS:
       if (Wclub.members.cache.has(membro.id)) {
         membro.roles.add(role);
       }
-    case Wclub:
+    case WC:
       if (Wstore.members.cache.has(membro.id)) {
         const membro2 = Wstore.members.cache.get(membro.id);
         membro2.roles.add(role);
