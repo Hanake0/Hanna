8642daab4f551d36c2161969a5e4310a8879dd95
Breno G H <bghaesee@outlook.com>
2020-12-03 16:48:30 -0300

    instalado Eslint

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import * as commando from './CommandoV12/src/index.js';
 import { HannaClient } from './src/Classes/hannaClient.js';
 import { readdirSync } from 'fs';
 import { Intents } from 'discord.js';
+import { oneLine } from 'common-tags';
 
 function hora() {
 	const dataUTC = new Date(new Date().toUTCString());
 	const dataBR = new Date(dataUTC.getTime() - 10800000);
-	let hora = `${dataBR.toISOString().slice(11, -1)} `;
-	return hora
+	const horaString = `${dataBR.toISOString().slice(11, -1)} `;
+	return horaString;
 }
 
 // Inicializa o banco de dados (firebase) e exporta o banco Online
@@ -16,21 +17,22 @@ const firebase = require('firebase/app');
 const FieldValue = require('firebase-admin').firestore.FieldValue;
 */
 import admin from 'firebase-admin';
+// eslint-disable-next-line no-unused-vars
 import serviceAccount from './serviceAccount.js';
 import serviceAccount2 from './serviceAccount2.js';
 
 admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount2)
+	credential: admin.credential.cert(serviceAccount2),
 });
 
 export const db = admin.firestore();
 
-//cria um client do Comando
+// Cria um client do Comando
 const donos = new Set();
-  donos.add('380512056413257729');
-  donos.add('348664615175192577');
-  donos.add('398852531259965440');
-  donos.add('755067822086029424');
+donos.add('380512056413257729');
+donos.add('348664615175192577');
+donos.add('398852531259965440');
+donos.add('755067822086029424');
 export const client = new HannaClient({
 	ws: { intents: Intents.ALL },
 	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
@@ -47,13 +49,13 @@ export const client = new HannaClient({
 		messageDeleteBulk: '779509870579810320',
 		ban: '779509870579810318',
 		channel: '779509870579810323',
-	}
+	},
 });
 
-client.setProvider( new commando.FirebaseProvider(db));
+client.setProvider(new commando.FirebaseProvider(db));
 
 
-//registra os comandos no client do Commando
+// Registra os comandos no client do Commando
 client.registry
 	.registerDefaults()
 	.registerGroups([
@@ -64,33 +66,33 @@ client.registry
 		['imgs-ia', 'Imagens Geradas com Inteligência Artificial'],
 		['p&i', 'Perfil & Inventário'],
 		['adm', 'Reservados á Staff'],
-		['eventos', 'Relacionados a Eventos']
+		['eventos', 'Relacionados a Eventos'],
 	])
 	.registerCommandsIn('./Comandos');
 
 
 // Importa o Manager do firestore e inicializa ele
 import { FirestoreManager } from './src/Firestore/base.js';
-const FirestoreMngr = new FirestoreManager(client, db)
+const FirestoreMngr = new FirestoreManager(client, db);
 
 FirestoreMngr.init();
 
 
-//Event Handler(Project-A) && erros
+// Event Handler(Project-A) && erros
 const evtFiles = readdirSync('./Eventos/');
 console.log(hora(), `Carregando o total de ${evtFiles.length} eventos`);
 evtFiles.forEach(async f => {
-  const eventName = f.split('.')[0];
-  const { default: event } = await import(`./Eventos/${f}`);
+	const eventName = f.split('.')[0];
+	const { default: event } = await import(`./Eventos/${f}`);
 
-  client.on(eventName, event.bind(null, client));
+	client.on(eventName, event.bind(null, client));
 });
 
 /*
 // Loja
 client.once('ready', () => {
 	const items = readdirSync('./src/Loja/Items');
-	const shopItens = client.registry.shopItens; 
+	const shopItens = client.registry.shopItens;
 
 	items.forEach(async item => {
 		let { default: itemConstructor } = await import(`./src/Loja/Items/${item}`);
@@ -102,7 +104,7 @@ client.once('ready', () => {
 client.on('messageReactionAdd', async (reaction, user) => {
 	if(user.id === client.user.id) return;
 
-	const shopItens = client.registry.shopItens; 
+	const shopItens = client.registry.shopItens;
 	const mID = reaction.message.id;
 	const item = shopItens.get(mID);
 
@@ -118,7 +120,7 @@ client.on('messageReactionAdd', async (reaction, user) => {
 					})
 			};
 		});
-		try { 
+		try {
 			console.log(hora(), `Loja => Executando item ${item.nome}`);
 			item.run(client, reaction, user);
 		} catch(err) {
@@ -128,23 +130,23 @@ client.on('messageReactionAdd', async (reaction, user) => {
 });
 */
 
-	client
-		.on('error', console.error)
-		.on('warn', console.warn)
-		.on('debug', (debug) => { if (!debug.includes('[WS => ')) console.log(hora(), debug); })
-		.on('disconnect', () => { console.warn('Disconectado!'); })
-		.on('reconnecting', () => { console.warn('Reconectando...'); })
-		.on('commandError', (cmd, err) => {
-			if(err instanceof commando.FriendlyError) return;
-			console.error(hora(), `Erro no comando ${cmd.groupID}:${cmd.memberName}`, err);
-		})
-		.on('commandBlocked', (msg, reason) => {
-			console.log(oneLine`
+client
+	.on('error', console.error)
+	.on('warn', console.warn)
+	.on('debug', (debug) => { if (!debug.includes('[WS => ')) console.log(hora(), debug); })
+	.on('disconnect', () => { console.warn('Disconectado!'); })
+	.on('reconnecting', () => { console.warn('Reconectando...'); })
+	.on('commandError', (cmd, err) => {
+		if(err instanceof commando.FriendlyError) return;
+		console.error(hora(), `Erro no comando ${cmd.groupID}:${cmd.memberName}`, err);
+	})
+	.on('commandBlocked', (msg, reason) => {
+		console.log(oneLine`
 				Comando ${msg.command ? `${msg.command.groupID}:${msg.command.memberName}` : ''}
 				bloqueado; ${reason}
 			`);
-		})
-		.on('firestoreDebug', (...infos) => console.log(infos.join('')));
+	})
+	.on('firestoreDebug', (...infos) => console.log(infos.join('')));
 
-//login && token
+// login && token
 client.login('NzQzOTgwMzgwNzU4OTk5MDgx.XzcjuQ.ocKx6z1nH65fme7svGi8V7Yd_V4');
