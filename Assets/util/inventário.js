19001218f4ce154fdd710d8f7586866b80578450
Breno G H <bghaesee@outlook.com>
2020-12-28 18:59:55 -0300

    remove conflictiong folders

--- Assets/util/inventário.js
+++
@@ -1,6 +0,0 @@
-module.exports = class Itens {
-  static cor(cor, membro) {
-    if(membro._roles.includes(cor.rID)) return 'Já está usando esta cor'
-    
-  }
-}
--- src/Classes/buddy.js
+++
@@ -1,8 +0,0 @@
-export class Buddy {
-	constructor(client, infos) {
-
-		Object.defineProperty(this, 'client', { value: client });
-
-		this.infos = infos;
-	}
-}
